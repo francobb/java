bc48c966be7f87a40d1f52b84d1147b4b2037f11
francobb <jrizzle1790@gmail.com>
2022-05-06 09:59:14 -0400

    db configuration

--- spring-boot-security-course/src/main/resources/db.migration/V1__init.sql
+++ spring-boot-security-course/src/main/resources/db.migration/V1__init.sql
@@ -3,5 +3,17 @@ CREATE TABLE IF NOT EXISTS player (
   first_name     varchar(50)    not null,
   last_name     varchar(50)    not null,
   height        varchar(4)     not null,
-  position      varchar(2)     not null
+  position      varchar(2)     not null,
+  stats_id      uuid           null ,
+  constraint player_stats_fkey
+        foreign key (stats_id) references stats (id)
+);
+
+CREATE TABLE IF NOT EXISTS stats (
+  id            uuid           not null primary key,
+  ppg           smallint       not null,
+  fgm           smallint       not null,
+  fga           smallint       not null,
+  fta           smallint       not null,
+  ftm           smallint       not null
 );
